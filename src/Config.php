4a9ae63398d0805d5635c31a73628bf0f74ab6b9
Tioncico <31308307+tioncico@users.noreply.github.com>
2018-10-12 13:38:11 +0800

    Update Config.php

--- src/Config.php
+++ src/Config.php
@@ -39,11 +39,12 @@ class Config
         return $this->ipWhiteList;
     }
 
-    function setIpWhiteList():IpWhiteList
+    function setIpWhiteList(ipWhiteList $ipWhiteList):IpWhiteList
     {
-        if(empty($this->ipWhiteList)){
-            $this->ipWhiteList = new IpWhiteList();
+        if(empty($ipWhiteList)){
+            $ipWhiteList= new IpWhiteList();
         }
+        $this->ipWhiteList = $ipWhiteList;
         return $this->ipWhiteList;
     }
 
